38f90f670dce5043ae49224a9277afc23ca92f43
TedMandingo <31971094+TedMandingo@users.noreply.github.com>
2019-02-03 00:41:14 +0300

    initial commit

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Duka Poa</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+    </head>
+    <body>
+        <div id="app">
+            <header class="with-background">
+                <div class="top-nav container">
+                    <div class="top-nav-left">
+                        <div class="logo">Duka Poa</div>
+                        {{ menu('main', 'partials.menus.main') }}
+                    </div>
+                    <div class="top-nav-right">
+                        @include('partials.menus.main-right')
+                    </div>
+                </div> <!-- end top-nav -->
+                <div class="hero container">
+                    <div class="hero-copy">
+                        <h1>Duka Poa</h1>
+                        <p>Online shop.</p>
+                        <div class="hero-buttons">
+                            
+                        </div>
+                    </div> <!-- end hero-copy -->
+
+                    <div class="hero-image">
+                        <img src="img/special.png" alt="hero image">
+                    </div> <!-- end hero-image -->
+                </div> <!-- end hero -->
+            </header>
+
+            <div class="featured-section">
+
+                <div class="container">
+                    {{-- <div class="tabs">
+                        <div class="tab">
+                            Featured
+                        </div>
+                        <div class="tab">
+                            On Sale
+                        </div>
+                    </div> --}}
+
+                    <div class="products text-center">
+                        @foreach ($products as $product)
+                            <div class="product">
+                                <a href="{{ route('shop.show', $product->slug) }}"><img src="{{ productImage($product->image) }}" alt="product"></a>
+                                <a href="{{ route('shop.show', $product->slug) }}"><div class="product-name">{{ $product->name }}</div></a>
+                                <div class="product-price">{{ $product->presentPrice() }}</div>
+                            </div>
+                        @endforeach
+
+                    </div> <!-- end products -->
+
+                    <div class="text-center button-container">
+                        <a href="{{ route('shop.index') }}" class="button">View more products</a>
+                    </div>
+
+                </div> <!-- end container -->
+
+            </div> <!-- end featured-section -->
+
+            <blog-posts></blog-posts>
+
+            @include('partials.footer')
+
+        </div> <!-- end #app -->
+        <script src="js/app.js"></script>
+    </body>
+</html>
